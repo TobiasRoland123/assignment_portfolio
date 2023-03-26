85ea2b0c94baf5d3846f7d04a70f5c7b31e9b692
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-03-26 19:57:38 +0200

    load animation and copy to all sites added

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-"use strict";
+import { animate, scroll, stagger, inView } from "https://cdn.skypack.dev/motion";
+
+animate("header h1", { opacity: [0, 1], y: [0, 40] }, { duration: 2.5 });
+animate("header p", { opacity: [0, 1], y: [0, 30] }, { duration: 2.5 });
